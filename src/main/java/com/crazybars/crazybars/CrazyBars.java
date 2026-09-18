68399d685ad0377fb634796143e86b15806244ce
Miles Baack <milesbaack@Miless-MacBook-Pro.local>
2026-09-18 14:47:52 -0700

    pre-merge

--- src/main/java/com/crazybars/crazybars/CrazyBars.java
+++ src/main/java/com/crazybars/crazybars/CrazyBars.java
@@ -5,7 +5,6 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
 import javafx.scene.shape.Line;
 
 // Import JavaFX into Intellij via manual download
